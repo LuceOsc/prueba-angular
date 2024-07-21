e9580fdfb4cf9565ae4c51ff4582b46ea50da877
LuceOsc <128515459+LuceOsc@users.noreply.github.com>
2024-07-21 12:52:31 -0700

    Prueba tecnica

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+// Se configura y conecta firabase
+export const environment = {
+  firebase: {
+    projectId: 'login-test-f521c',
+    appId: '1:733027166526:web:8e7e0de156754f3ad1646f',
+    storageBucket: 'login-test-f521c.appspot.com',
+    apiKey: 'AIzaSyDcQ3FKdafqmLFKauA2tEoPgTp_rnOyQTs',
+    authDomain: 'login-test-f521c.firebaseapp.com',
+    messagingSenderId: '733027166526',
+  },
+};
